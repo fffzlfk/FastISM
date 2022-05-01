83206b1ef56bd027c0406de4dea6f5591e41f976
fffzlfk <1319933925qq@gmail.com>
2022-05-01 11:21:39 +0800

    tests: fix test

--- test_sizes.py
+++ test_sizes.py
@@ -9,5 +9,5 @@ def run_command(cmd: str):
 
 if __name__ == "__main__":
     func = sys.argv[1]
-    for i in range(10, 16):
-        run_command(f"./build/{ func } images/{ 1 << i }x{ 1 << i }.png")
+    for i in range(8, 14):
+        run_command(f"./build/{ func } images/sizes/{ 1 << i }.png")
